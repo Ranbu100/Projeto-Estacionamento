fd22532686f1c3cbe85aa487fc601733dae5566c
Renan <renan.mota2534@gmail.com>
2024-09-23 08:32:37 -0300

    commit teste

--- frontend/web/src/components/itens/itemMenu.tsx
+++ frontend/web/src/components/itens/itemMenu.tsx
@@ -11,7 +11,7 @@ export function ItemMenu({nome, route} : Props){
     const Router = useRouter();
     return (
         <Button variant="link"
-        className="flex items-center gap-1 sm:gap-2"
+        className="flex items-center  gap-1 sm:gap-2"
         onClick={() => Router.push(route)}>
             <span className="text-white text-xs">{nome}</span>
         </Button>
